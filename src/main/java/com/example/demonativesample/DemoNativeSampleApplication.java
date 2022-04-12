c2856e661ba48f99f8c5919c0090c670dafb1b69
Stephane Nicoll <snicoll@vmware.com>
2022-04-12 09:06:02 +0200

    Initial commit

---
+++ src/main/java/com/example/demonativesample/DemoNativeSampleApplication.java
@@ -0,0 +1,22 @@
+package com.example.demonativesample;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoNativeSampleApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(DemoNativeSampleApplication.class)
+				// TODO: this is necessary as the deduce algorithm does not work in native
+				.main(DemoNativeSampleApplication.class)
+				.run(args);
+	}
+
+	@Bean
+	public String one() {
+		return "one";
+	}
+
+}
