f409dad0c31b3b6e0886d0e085745f1355a3a55f
Stephane Nicoll <snicoll@vmware.com>
2022-04-26 16:55:07 +0200

    Switch to web sample

---
+++ src/main/java/com/example/demonativesample/TomcatRuntimeHintsRegistrar.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.demonativesample;
+
+import org.apache.catalina.startup.Tomcat;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.boot.web.embedded.tomcat.TomcatEmbeddedWebappClassLoader;
+
+/**
+ * Registration of runtime hints for Tomcat.
+ *
+ * @author Stephane Nicoll
+ */
+class TomcatRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
+
+	@Override
+	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+		ReflectionHints reflection = hints.reflection();
+		reflection.registerType(TomcatEmbeddedWebappClassLoader.class, (hint) -> {
+			hint.withMembers(MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+			hint.onReachableType(TypeReference.of(Tomcat.class));
+		});
+	}
+
+}
