74c20f8254363ecf0076cf1a489beb44ad37cfeb
Stephane Nicoll <snicoll@vmware.com>
2022-05-20 16:34:31 +0200

    Adapt to latest improvements

--- src/main/java/com/example/demonativesample/DemoNativeSampleApplication.java
+++ src/main/java/com/example/demonativesample/DemoNativeSampleApplication.java
@@ -4,17 +4,14 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(TomcatRuntimeHintsRegistrar.class)
 public class DemoNativeSampleApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(DemoNativeSampleApplication.class)
 				// TODO: this is necessary as the deduce algorithm does not work in native
 				.web(WebApplicationType.SERVLET)
-				.main(DemoNativeSampleApplication.class)
 				.run(args);
 	}
 
